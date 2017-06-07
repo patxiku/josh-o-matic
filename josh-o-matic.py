1c67c278cbf269a2c99ea672cda0f791a01e124f
fat-animal <fatanimal20@gmail.com>
2017-06-07 21:06:15 +0200

    Use proxies

--- josh-o-matic.py
+++ josh-o-matic.py
@@ -1,6 +1,6 @@
 #Josh-omatic improved so results seem real
 import requests
-from random import randint
+import random
 from time import sleep
  
 api = "http://www.matthewlush.com/VotingSystem/submitVote.php"
@@ -8,7 +8,19 @@ votecount = 0
 count = 0
 whovote = 7
 name = "Josh"
+
+def randomHttpProxy():
+    lines = open('proxies.txt').read().splitlines()
+    myline =random.choice(lines)
+    return myline
+
+
+def randomIp():
+    return str(random.randint(90,180))+"."+str(random.randint(90,180))+"."+str(random.randint(90,180))+"."+str(random.randint(90,180))
+
 while True:
+
+
     if count <= 3:
         whovote = 7
         name = "Josh"
@@ -19,16 +31,33 @@ while True:
         whovote = 5
         name = "Gaylord with id 5"
         count = 0
-        
+       
     votecount+= 1
-    fakeip = str(randint(90,180))+"."+str(randint(90,180))+"."+str(randint(90,180))+"."+str(randint(90,180))
-    postvars = { "postperson":whovote,"postip":fakeip }
- 
-    x = requests.post(api,params=postvars)
+    
+    proxy = randomHttpProxy()
+
+    if proxy:
+        ip,port = proxy.split(":")
+        print ( "Proxy with IP " + ip + " will be used")
+        postvars = { "postperson":whovote,"postip":randomIp() }
+        try:
+            x = requests.post(api,params=postvars,proxies={'http' : proxy})
+        except requests.exceptions.ProxyError:
+            continue
+    else: 
+        ip = randomIp()
+        print ( "Random IP " + ip + " will be used")
+        postvars = { "postperson":whovote,"postip":randomIp() }
+        x = requests.post(api,params=postvars)  
+
  
     if (x.text == "Thank you for voting!"):
         print (name +" got another vote, "+ str(votecount) + " times voted.")
-    sleepytime = randint(5,10)
+    
+    sleepytime = random.randint(0,5)
+
     print ("Waiting for " + str(sleepytime) + " seconds")
+
     sleep(sleepytime)
+    
     count += 1
