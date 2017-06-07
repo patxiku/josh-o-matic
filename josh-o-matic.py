9302c5f3f8acb7dd9ba247e5cbb22cfcdaeb3de7
Julen <julenupload@gmail.com>
2017-06-07 22:08:42 +0200

    Version 0.1
    Didn't know that github merges fucked things like this

--- josh-o-matic.py
+++ josh-o-matic.py
@@ -1,8 +1,7 @@
 #Josh-o-matic v0.1
 #Contributors: Patxiku, Fat-animal
-
+#---------------------------------
 import requests
-<<<<<<< HEAD
 import csv
 import random
 from time import sleep
@@ -88,66 +87,3 @@ while True:
 
 
 
-=======
-import random
-from time import sleep
- 
-api = "http://www.matthewlush.com/VotingSystem/submitVote.php"
-votecount = 0
-count = 0
-whovote = 7
-name = "Josh"
-
-def randomHttpProxy():
-    lines = open('proxies.txt').read().splitlines()
-    myline =random.choice(lines)
-    return myline
-
-
-def randomIp():
-    return str(random.randint(90,180))+"."+str(random.randint(90,180))+"."+str(random.randint(90,180))+"."+str(random.randint(90,180))
-
-while True:
-
-
-    if count <= 3:
-        whovote = 7
-        name = "Josh"
-    elif count <= 5:
-        whovote = 3
-        name = "Gaylord with id 3"
-    else:
-        whovote = 5
-        name = "Gaylord with id 5"
-        count = 0
-       
-    votecount+= 1
-    
-    proxy = randomHttpProxy()
-
-    if proxy:
-        ip,port = proxy.split(":")
-        print ( "Proxy with IP " + ip + " will be used")
-        postvars = { "postperson":whovote,"postip":randomIp() }
-        try:
-            x = requests.post(api,params=postvars,proxies={'http' : proxy})
-        except requests.exceptions.ProxyError:
-            continue
-    else: 
-        ip = randomIp()
-        print ( "Random IP " + ip + " will be used")
-        postvars = { "postperson":whovote,"postip":randomIp() }
-        x = requests.post(api,params=postvars)  
-
- 
-    if (x.text == "Thank you for voting!"):
-        print (name +" got another vote, "+ str(votecount) + " times voted.")
-    
-    sleepytime = random.randint(0,5)
-
-    print ("Waiting for " + str(sleepytime) + " seconds")
-
-    sleep(sleepytime)
-    
-    count += 1
->>>>>>> origin/master
